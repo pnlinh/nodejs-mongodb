c3ed73512dbc797fab47ec6fc7fbf5a67a387931
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-20 18:28:16 +0700

    paginate

--- database/userController.js
+++ database/userController.js
@@ -22,4 +22,42 @@ const deleteAllUsers = async () => {
     }
 };
 
-module.exports = {insertUser, deleteAllUsers};
+const findUserById = async (userId) => {
+    try {
+        let foundUser = await User.findById(userId);
+        console.log(`foundUser = ${JSON.stringify(foundUser)}`);
+    } catch (error) {
+        console.log(`User not found: ${error}`);
+    }
+};
+
+const findSomeUsers = async () => {
+    try {
+        let foundUsers = await User.find(
+            {}, // Show all users
+            // {age: {$gte: 24}, name: /linh/i}, // Insensitive-case
+            ['name', 'email', 'age'], // Show only name, email
+            // Sort ?
+            {
+                sort: {
+                    name: 1 // 1: Alphabetically, -1: Reverse Alphabetically
+                }
+            }
+        ).skip(2 * 5).limit(5);
+
+        foundUsers.forEach(user => {
+            console.log(`User = ${JSON.stringify(user)}`);
+        });
+
+        console.log(`Sum count record found: ${foundUsers.length}`);
+    } catch (error) {
+        console.log(`User not found: ${error}`);
+    }
+};
+
+module.exports = {
+    insertUser,
+    deleteAllUsers,
+    findUserById,
+    findSomeUsers
+};
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const {insertUser, deleteAllUsers} = require('./database/userController');
+const {insertUser, deleteAllUsers, findUserById, findSomeUsers} = require('./database/userController');
 
-insertUser('pnlinh', 24, 'pnlinh1207@gmail.com');
-insertUser('pnlinh2', 25, 'pnlinh1207abc@gmail.com');
-insertUser('pnlinh3', 26, 'pnlinh1207xyz@gmail.com');
+// insertUser('pnlinh', 24, 'pnlinh1207@gmail.com');
+// insertUser('pnlinh2', 25, 'pnlinh1207abc@gmail.com');
+// insertUser('pnlinh3', 26, 'pnlinh1207xyz@gmail.com');
 
 // Becareful
 // deleteAllUsers();
+
+// findUserById('5c921af2a522553ecd90db57');
+
+findSomeUsers();
